bc23235e397bae909aeae37d7e8c4e65dfc40542
Willem Melching <willem.melching@gmail.com>
2020-10-30 14:25:05 +0100

    load elf into mmu

--- remu/src/main.rs
+++ remu/src/main.rs
@@ -1,4 +1,20 @@
+pub mod mmu;
+
+use std::path::PathBuf;
+use clap::{Arg, App};
+use mmu::{Mmu, VirtAddr};
+
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Emulator")
+        .arg(Arg::with_name("input")
+             .long("input").
+             takes_value(true))
+        .get_matches();
+
+    let myfile = matches.value_of("input").unwrap();
+    let path = PathBuf::from(myfile);
+
+    let mut mmu = Mmu::new(1024 * 1024);
+    mmu.load_elf(&path)
 }
---
+++ remu/src/mmu.rs
@@ -0,0 +1,73 @@
+use std::path::PathBuf;
+
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct VirtAddr(pub usize);
+
+pub struct Mmu {
+    memory: Vec<u8>,
+    cur_alloc: VirtAddr,
+}
+
+impl Mmu {
+    pub fn new(size: usize) -> Self {
+        Mmu {
+            memory: vec![0; size],
+            cur_alloc: VirtAddr(0),
+        }
+    }
+
+    pub fn alloc(&mut self, size: usize) -> VirtAddr {
+        let r = self.cur_alloc;
+        self.cur_alloc = VirtAddr(self.cur_alloc.0 + size);
+        r
+    }
+
+    pub fn read(&self, addr: VirtAddr, size: usize) -> Vec<u8> {
+        let mut buf = vec![0; size];
+        self.read_into(addr, &mut buf);
+        buf
+    }
+
+    pub fn read_into(&self, addr: VirtAddr, buf: &mut [u8]) {
+        buf.copy_from_slice(&self.memory[addr.0..addr.0 + buf.len()]);
+    }
+
+    pub fn write_from(&mut self, addr: VirtAddr, buf: &[u8]) {
+        self.memory[addr.0..addr.0 + buf.len()].copy_from_slice(buf);
+    }
+
+    pub fn load_section(&mut self, section: &elf::Section) {
+        let header = &section.shdr;
+        let addr = header.addr as usize;
+        let size = header.size as usize;
+
+        self.write_from(VirtAddr(addr), &section.data);
+        self.cur_alloc = VirtAddr(std::cmp::max(self.cur_alloc.0, addr + size));
+    }
+
+    pub fn load_elf(&mut self, path : &PathBuf) {
+        let file = elf::File::open_path(&path).unwrap();
+
+        for section in &file.sections {
+            if section.shdr.addr > 0 {
+                self.load_section(&section);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_write() {
+        let dat = vec![1, 2, 3, 4];
+        let addr = VirtAddr(0);
+        let mut mmu = Mmu::new(128);
+
+        mmu.write_from(addr, &dat);
+        assert_eq!(dat, mmu.read(addr, dat.len()));
+    }
+}
